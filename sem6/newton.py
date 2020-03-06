7397e4d83f2ea59f206c88072c974cda56655586
Artemy <artemymif@gmail.com>
2020-03-06 22:08:00 +0300

    Commit

--- sem6/newton.py
+++ sem6/newton.py
@@ -4,19 +4,33 @@ from pylab import *
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-# Вариант Ньютона-Котса
+a = 0
+b = 2.2
+alpha = 0.2
+betta = 0
 
-a = float(input("Введите левую границу:"))
-b = float(input("Введите правую границу:"))
+
+def f11(x):
+    y = 2.5 * math.cos(2 * x) * math.exp(2 * x / 3) + 4 * math.sin(3.5 * x) * math.exp(-3 * x) + 3 * x
+    return y
+
+
+def f22(x):
+    if x-a == 0:
+        y = 1
+    else:
+        y = ((x - a) ** (-alpha)) * ((b - x) ** (-betta))
+    return y
 
 
 def f(x):
-    #return 2.5 * math.cos(2 * x) * math.exp(2 * x / 3) + 4 * math.sin(3.5 * x) * math.exp(-3 * x) + 3 * x
-    return 2 * math.cos(3.5 * x) * math.exp(5 * x / 3) + 3 * math.sin(1.5 * x) * math.exp(-4 * x) + 3
+    y = f1(x)
+    return y
+
 
 def f1(x):
-    #return (2.5 * math.cos(2 * x) * math.exp(2 * x / 3) + 4 * math.sin(3.5 * x) * math.exp(-3 * x) + 3 * x) / ((x - 0.1) ** 0.2)
-    return (2 * math.cos(3.5 * x) * math.exp(5 * x / 3) + 3 * math.sin(1.5 * x) * math.exp(-4 * x) + 3) / ((x - 1.5) ** 0.2)
+    y = f11(x) * f22(x)
+    return y
 
 # Точное значение:
 exv = integrate.quad(f1, a, b)[0]
@@ -25,20 +39,19 @@ out = []
 outcheb = []
 scout = []
 scoutcheb = []
-for l in range(3, 20):
-    ##############
+for l in range(3, 25):
     cin.append(l)
     n = l
-    x = []
-    cheb = []
+    x = [0 for i in range(n)]
+    cheb = [0 for i in range(n)]
     delta = (b - a) / (n - 1)
     for i in range(0, n):
-        x = numpy.append(x, a + i * delta)
-        cheb = numpy.append(cheb, 1/2*((b-a)*math.cos((2*i+1)/(2*(n+1)*math.pi))+(b+a)))
-    moments = []
+        x[i] = a + i * delta
+        cheb[i] = 1/2*((b-a)*math.cos((2*i+1)/(2*(n+1)*math.pi))+(b+a))
 
+    moments = [0 for i in range(0, n)]
     for i in range(0, n):
-        moments = numpy.append(moments, (2.2 ** (0.8 + i)) / (i + 0.8))
+        moments[i] = (2.2 ** (0.8 + i)) / (i + 0.8)
     matrix = [[0 for i in range(n)] for j in range(n)]
     matrixcheb = [[0 for i in range(n)] for j in range(n)]
     for i in range(0, n):
@@ -63,8 +76,6 @@ for l in range(3, 20):
     outcheb.append(-math.log10(abs(scheb - exv)))
     scout.append(ss)
     scoutcheb.append(sscheb)
-    #############
-
 
 plt.subplot(221)
 plt.plot(cin, out)
